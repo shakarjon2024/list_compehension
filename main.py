323466dc82398e8be9cd319b5ceadfb503113401
shakarjon2024 <rozibayevashakarjon1707@gmail.com>
2025-12-09 16:05:26 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+#  1 - misol
+r = [x**2 for x in range(1, 11)]
+print(r)
+
+
+# 2 - misol
+num = [x for x in range(1, 21) if x % 2 == 0]
+print(num)
+
+
+# 3 - misol
+num = [x for x in range(10, 30) if x % 2 == 1]
+print(num)
+
+
+# 4 - misol
+soz_uzunligi = ["apple", "banana", "pear"]
+res = [len(x) for x in soz_uzunligi]
+print(res)
+
+
+# 5 - misol
+soz = 'so`z'
+result = [s for s in soz]
+print(result)
+
+
+# 6 - misol
+r = [x * 3 for x in range(1, 6)]
+print(r)
+
+
+
